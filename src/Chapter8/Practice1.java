9bab15544e995a57d9fef3c29293df6eb0b9242b
APSODE <2gunbo74@gmail.com>
2023-04-20 10:07:38 +0900

    실습문제 (1) 출제 의도 반영으로 인한 코드 수정

--- src/Chapter8/Practice1.java
+++ src/Chapter8/Practice1.java
@@ -9,7 +9,11 @@ public class Practice1 {
         for (int idx = 0; idx < randint_array.length; idx++) {
             int random_num = rnd.nextInt(100);
             randint_array[idx] = random_num;
-            System.out.print(random_num);
+
+        }
+
+        for (int idx = 0; idx < randint_array.length; idx++) {
+            System.out.print(randint_array[idx]);
         }
     }
 }
