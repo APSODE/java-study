b916a7704604fa162a054da8c63f96412ff1c91a
APSODE <2gunbo74@gmail.com>
2023-04-27 01:15:16 +0900

    중간고사 대비 별찍기 문제 풀이 2442번 / 2443번 / 2444번 / 2446번

---
+++ src/Midterm/ExamPrepare2.java
@@ -0,0 +1,25 @@
+package Midterm;
+
+import java.util.Scanner;
+
+public class ExamPrepare2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line_amount = sc.nextInt();
+
+        for (int line_num = line_amount; line_num >= 1; line_num--) {
+            int star_amount = 2 * line_num - 1;
+            int blank_amount = line_amount - line_num;
+
+            for (int blank = 0; blank < blank_amount; blank++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 0; star < star_amount; star++) {
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+    }
+}
